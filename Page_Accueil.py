b17a38b527dcc625762c003ff458f58c17d103c4
pierregabriel <74655603+pierregabriel@users.noreply.github.com>
2025-04-28 17:10:42 +0200

    Update Page_Accueil.py

--- Page_Accueil.py
+++ Page_Accueil.py
@@ -19,14 +19,12 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Stratégies d'Options")
-    st.image("https://via.placeholder.com/300x200.png?text=Stratégies+d'Options", use_column_width=True)
     if st.button("Accéder aux Stratégies d'Options"):
         # Redirection vers la page Stratégies
         st.switch_page("pages/1_Stratégies_Options.py")
 
 with col2:
     st.subheader("Pricing d'Options")
-    st.image("https://via.placeholder.com/300x200.png?text=Pricing+d'Options", use_column_width=True)
     if st.button("Accéder au Pricing d'Options"):
         # Redirection vers la page Pricing
         st.switch_page("pages/2_Pricing_options.py")
