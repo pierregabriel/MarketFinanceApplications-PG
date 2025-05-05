428576b3de7825efde39ecbef06fa210439007e7
pierregabriel <74655603+pierregabriel@users.noreply.github.com>
2025-05-05 16:06:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ page_dict = {
     "Accueil": [st.Page("pages/Page_Accueil.py", title="Accueil", icon="🏠")],
     "FX": [st.Page("pages/FX/FX.py", title="Marché FX", icon="💱")],
     "Options": [
-        st.Page("pages/options/Grecs.py", title="Grecques Options", icon="📊"),
-        st.Page("pages/options/Pricing_options.py", title="Pricing Options", icon="💹"),
-        st.Page("pages/options/Stratégie_options.py", title="Stratégies Options", icon="🔄")
+        st.Page("pages/options/Grecs.py", title="Greeks Visualizer"),
+        st.Page("pages/options/Pricing_options.py", title="Pricer"),
+        st.Page("pages/options/Stratégie_options.py", title="Strategies")
     ]
 }
 
