4ef4b644d8901bf922a1922007945caa39edc26e
pierregabriel <74655603+pierregabriel@users.noreply.github.com>
2025-08-11 14:28:44 +0200

    Update FX.py

--- pages/FX/FX.py
+++ pages/FX/FX.py
@@ -6,7 +6,7 @@ import plotly.express as px
 from datetime import datetime, timedelta
 import math
 from scipy.interpolate import interp1d
-from scrapping import collect_all_financial_data
+from Scrapping import collect_all_financial_data
 
 # Utility Functions
 @st.cache_data(ttl=300)  # Cache for 5 minutes to avoid too frequent scraping
